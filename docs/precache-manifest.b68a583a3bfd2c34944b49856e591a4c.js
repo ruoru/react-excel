f8786f01baae515d8229f69e42fcba6d8afcf27e
ruoru <cruoru@gmail.com>
2019-02-21 00:47:32 +0800

    1

---
+++ docs/precache-manifest.b68a583a3bfd2c34944b49856e591a4c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "7f7b8f18b3711b32a7dc",
+    "url": "/static/js/main.7f7b8f18.chunk.js"
+  },
+  {
+    "revision": "494de111ca5ed15595ba",
+    "url": "/static/js/2.494de111.chunk.js"
+  },
+  {
+    "revision": "7f7b8f18b3711b32a7dc",
+    "url": "/static/css/main.66ba9cd7.chunk.css"
+  },
+  {
+    "revision": "56fa354778e01d1a6e4ec76070253ab8",
+    "url": "/index.html"
+  }
+];
